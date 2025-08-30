a8bd326ecb2805b7912e3f77bc1c89cdace7e171
djomgaingmoyo <franckmoyo2002@gmail.com>
2025-08-30 13:43:42 -0400

    news changes

--- AppTontine/views.py
+++ AppTontine/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def Cotiser(request):
+    return render(request, 'Cotiser.html')
