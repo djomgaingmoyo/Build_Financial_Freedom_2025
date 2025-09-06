4acf9de81f56aae54fb6197bf6a09e4c69a0c4a0
djomgaingmoyo <franckmoyo2002@gmail.com>
2025-09-06 10:54:24 -0400

    commit changes

--- AppTontine/urls.py
+++ AppTontine/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from .views import *
 
+# (Url, fonction, nom de la page html)
 urlpatterns = [
     path('',home,name='home'),
     path('Cotiser/',Cotiser,name='Cotiser'),
