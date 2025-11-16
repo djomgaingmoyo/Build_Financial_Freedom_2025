663b88e610aafbca3a749cc3a488ffd8d0c53cc0
djomgaingmoyo <franckmoyo2002@gmail.com>
2025-11-15 20:53:11 -0500

    uuu

--- AppTontine/views.py
+++ AppTontine/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.views.generic import ListView
+from datetime import datetime
 
 from .models import *
 
@@ -14,12 +15,15 @@ def Cotiser(request):
         # recuperation des couples et du payeur dans les tables (Couple,Membre) en fonction de la cle primaire 
         couple_beneficiaire=Couple.objects.get(pk=request.POST['C_beneficiaire'])
         payeur=Membre.objects.get(pk=request.POST['payeur'])
+        mois_en_cours = datetime.now().month
+        annee_en_cours = datetime.now().year
+        tontine = Tontine.objects.get(annee=annee_en_cours)
 
         montant_total=request.POST['montantTotal']
         montant_membre1=request.POST['montantMembre1']
         montant_membre2=request.POST['montantMembre2']
 
-        savedonnees = Cotisation(couple_beneficiaire=couple_beneficiaire,payeur=payeur,montant_total=montant_total,montant_membre1=montant_membre1,montant_membre2=montant_membre2)
+        savedonnees = Cotisation(couple_beneficiaire=couple_beneficiaire,payeur=payeur,mois=mois_en_cours,tontine=tontine,montant_total=montant_total,montant_membre1=montant_membre1,montant_membre2=montant_membre2)
         savedonnees.save()
         return redirect('home')
     else:
